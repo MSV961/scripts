f171ef907b8b472737973f0e31ba4890a8abea54
Sergey <sergeymilyaev961@gmail.com>
2024-08-26 13:10:57 +0300

    Update tg.sh

--- scripts/tg_easy_monitoring/tg.sh
+++ scripts/tg_easy_monitoring/tg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CHAT_ID="735870138" # id чата куда отправляем
+CHAT_ID="" # id чата куда отправляем
 TEXT="$1" # текст который отправляем
-BOT_AUTH_TOKEN="7222015453:AAHxZx1zZpHaVkuXMAZJPrVHGDpndCrQbBw" # Токен бота
+BOT_AUTH_TOKEN="" # Токен бота
 
 # Передаем все данные в curl, которым сделаем запрос к API Telegram на отправку сообщения.
 curl -sS -i --max-time 30 \
