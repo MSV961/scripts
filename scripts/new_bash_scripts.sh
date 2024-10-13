0fa809a32af9131d47c7a8ac583bda6bb39bbe76
Sergey <sergeymilyaev961@gmail.com>
2024-10-13 14:38:23 +0300

    Update new_bash_scripts.sh

--- scripts/new_bash_scripts.sh
+++ scripts/new_bash_scripts.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
+
+# How to use: sudo bash new_bash_scripts.sh [name of file]
+# This sctipt may use for create new scripts 
+# this is ckeck that there is no file with the same name, and that create new file
 set -e
-set -u
 
 filename=$1
 
